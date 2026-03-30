bb6479b4a800e0cc044e12ae86a03b16883c0689
looted232 <xynconnect@gmail.com>
2026-03-30 18:14:03 -0500

    Update loadstring URLs for SAB script

--- starx.lua
+++ starx.lua
@@ -2,7 +2,7 @@ local placeId = game.PlaceId
 
 -- SAB script (ID: 109983668079237)
 if placeId == 109983668079237 then
-    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/XXc3cu6sNbdO2pkz/raw"))()
+    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/cXXyc3uvxqzKG6wA/raw"))()
 
 -- Escape Tsunami script (ID: 131623223084840)
 elseif placeId == 131623223084840 then
